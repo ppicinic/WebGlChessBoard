0867b6f2cca41b563dbdf14b82381a9271e8fa6a
Phil Picinic <PhilPicinic@gmail.com>
2013-11-28 15:12:38 -0500

    adding easing to Pawn piece

--- js/lib/easing.js
+++ js/lib/easing.js
@@ -15,3 +15,10 @@ function easeInSin(t, b, c, d) {
 function easeOutSin(t, b, c, d) {
 	return c * Math.sin(t/d * (Math.PI/2)) + b;
 }
+
+function easeInOutQuad(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+}
