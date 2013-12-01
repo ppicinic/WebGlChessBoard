213b2dc781f63a0002159921666b04dad06d31af
AnthonyB28 <anthony@lssclan.org>
2013-12-01 03:54:18 -0500

    Black Textures
    For the black people. Err pawns.

--- js/ChessBoard.js
+++ js/ChessBoard.js
@@ -80,32 +80,42 @@ ChessBoard.prototype.loadPieces = function(){
 	for(var i = 0; i < this.pieces.length; i++){
 		this.pieces[i] = new Array(8);
 	}
+	
+	var blackTex = THREE.ImageUtils.loadTexture('Models/textures/blackmarble1.jpg');
+
 	var piece;
 	for(var x = 0; x < this.pieces.length; x++){
 		for(var y = 0; y < this.pieces[x].length; y++){
 			if(y == 0){
 				if(x == 0 || x == 7){
 					piece = new Rook(this.scene, BLACK, [x, y], this);
+					piece.piece.children[0].children[0].material.map = blackTex;
 					this.pieces[x][y] = piece;
 				}
 				if(x == 1 || x == 6){
 					piece = new Knight(this.scene, BLACK, [x, y], this);
+					piece.piece.children[0].children[0].material.map = blackTex;
 					this.pieces[x][y] = piece;
 				}
 				if(x == 2 || x == 5){
 					piece = new Bishop(this.scene, BLACK, [x, y], this);
+					piece.piece.children[0].children[0].material.map = blackTex;
 					this.pieces[x][y] = piece;
 				}
 				if(x == 3){
 					piece = new Queen(this.scene, BLACK, [x, y], this);
+					piece.piece.children[0].children[0].material.map = blackTex;
 					this.pieces[x][y] = piece;
 				}
 				if(x == 4){
 					piece = new King(this.scene, BLACK, [x, y], this);
+					piece.piece.children[1].children[0].material.map = blackTex;
+					piece.piece.children[1].children[1].material.map = blackTex;
 					this.pieces[x][y] = piece;
 				}
 			}else if(y == 1){
 				piece = new Pawn(this.scene, BLACK, [x,y], this);
+				piece.piece.children[0].children[0].material.map = blackTex;
 				this.pieces[x][y] = piece;
 			}else if(y == 6){
 				piece = new Pawn(this.scene, WHITE, [x,y], this);
