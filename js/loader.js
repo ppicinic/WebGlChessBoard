f755db237119cf003aea69b7c800382ed7a6b6e0
Phil Picinic <PhilPicinic@gmail.com>
2013-12-03 04:01:38 -0500

    moved UI into its own class

--- js/loader.js
+++ js/loader.js
@@ -5,55 +5,7 @@
 			var test;
 			
 			//Needed for GUI
-			var cameraFolder, serverFolder, themeFolder; 
-			var guiServParams = { //Needed for GUI, has server settings and Reset. Default settings.
-				ServSetting: false, //True for GameID connection
-				ServerUrl: "http://test.com",
-				GameID: "team03",
-				Connect: function(){
-				//TODO: Connect to server
-					if(!this.ServSetting)
-					{
-						console.log("Connect to " + this.ServerUrl);
-					}
-					else {
-					console.log("Connect to ID: " + this.GameID);
-					}
-				},
-				Close: function(){
-				 //TODO:Close connection
-					console.log("Close");
-				},
-				Reset: function()
-				{
-					guiServParams.ServerUrl = "http://test.com";
-					guiServParams.GameID = "team03";
-					guiThemeParams.type = "Marble";
-					guiThemeParams.type = "High";
-					guiServParams.Close();
-					//TODO: Reset chess pieces
-					console.log("Reset here");
-					serverFolder.close();
-					serverFolder.__controllers[1].remove();
-					serverFolder.__controllers[2].remove();
-					serverFolder.__controllers.splice(1,2);
-
-					serverFolder.add(guiServParams,'ServerUrl');
-					serverFolder.add(guiServParams,'Connect').onFinishChange(function(){ 
-					serverFolder.__controllers[2].remove();
-					serverFolder.__controllers.splice(2);
-					serverFolder.close(); 
-					serverFolder.add(guiServParams,'Close').name("Close Connection");}
-						);
-				}
 			
-			};
-			
-			var guiThemeParams = { //Needed for GUI, had theme settings.
-			//TODO:Functions here.
-			type: "Marble",
-			quality: "High"
-			};
 			
 			var mouseX = 0, mouseY = 0;
 
@@ -61,7 +13,8 @@
 			var windowHalfY = window.innerHeight / 2;
 			var startTime;
 			var endTime;
-
+			var datdatgui;
+			
 			init();
 			var board;
 			var movep = -1;
@@ -115,9 +68,12 @@
 				
 
 				// scene
-
+		
 				scene = new THREE.Scene();
-
+				
+				datdatgui = new UIController();
+				datdatgui.gui();
+				
 				var ambient = new THREE.AmbientLight( 0x00000f );
 				scene.add( ambient );
 				var areaLight1 = new THREE.AreaLight( 0xffffff, 100 );
@@ -382,83 +338,5 @@
 //Create serverFolder, themeFolder, cameraFolder vars global
 //Needs the vars at the top like guiServParams for the typing. Cannot just create blank buttons.
 function gui(){
-    var gui = new dat.GUI();
-    serverFolder = gui.addFolder('Server');
-	
-	
-	serverFolder.add(guiServParams,'ServSetting').name('Use GameID?').onFinishChange(function(){
-		if(guiServParams.ServSetting)
-		{
-			serverFolder.__controllers[1].remove();
-			serverFolder.__controllers[2].remove();
-			serverFolder.__controllers.splice(1,2);
-			serverFolder.add(guiServParams,'GameID');
-			serverFolder.add(guiServParams,'Connect').onFinishChange(function(){ 
-			serverFolder.__controllers[2].remove();
-			serverFolder.__controllers.splice(2);
-			
-			serverFolder.close(); 
-			serverFolder.add(guiServParams,'Close').name("Close Connection");});
-		}
-		else
-		{
-			console.log(serverFolder);
-			serverFolder.__controllers[1].remove();
-			serverFolder.__controllers[2].remove();
-			serverFolder.__controllers.splice(1,2);
-
-			serverFolder.add(guiServParams,'ServerUrl');
-			serverFolder.add(guiServParams,'Connect').onFinishChange(function(){ 
-			serverFolder.__controllers[2].remove();
-			serverFolder.__controllers.splice(2);
-			serverFolder.close(); 
-			serverFolder.add(guiServParams,'Close').name("Close Connection");}
-				);
-		}
-		
-	});
-		
-		
-	serverFolder.add(guiServParams,'ServerUrl');
-    serverFolder.add(guiServParams,'Connect').onFinishChange(function(){ 
-			serverFolder.__controllers[2].remove();
-			serverFolder.__controllers.splice(2);
-			
-			serverFolder.close(); 
-			serverFolder.add(guiServParams,'Close').name("Close Connection");}
-	);
     
-    serverFolder.open();
-	
-    cameraFolder = gui.addFolder('Camera');
-    cameraFolder.add(camera.position, 'x', -500,500).step(5);
-	cameraFolder.add(camera.position, 'y', -500,500).step(5);
-    cameraFolder.add(camera.position, 'z', -500,500).step(5);
-	
-	themeFolder = gui.addFolder('Themes');
-    themeFolder.add(guiThemeParams, 'type', ["Marble","Wood"]).name("Piece Type:").onFinishChange(function(){
-			if(guiThemeParams.type=="Wood")
-			{
-				//Make models wood.
-				console.log("wood");
-			}
-			else
-			{
-			//Make models Marble.
-			console.log("Marble");
-			}
-		});
-	themeFolder.add(guiThemeParams, 'quality', ["High","Low"]).name("Quality:").onFinishChange(function(){
-			if(guiThemeParams.quality=="Low")
-			{
-				//Make models low.
-				console.log("low");
-			}
-			else
-			{
-			//Make models high.
-			console.log("high");
-			}
-		});
-	gui.add(guiServParams,'Reset');
 }
