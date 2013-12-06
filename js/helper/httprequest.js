89063135a695e9fc08976947492de12a072095ec
Phil Picinic <PhilPicinic@gmail.com>
2013-12-06 17:31:13 -0500

    moved easing library added ajax request library as well

---
+++ js/helper/httprequest.js
@@ -0,0 +1,12 @@
+// From Eloquent Javascript
+
+function makeHttpObject() {
+  try {return new XMLHttpRequest();}
+  catch (error) {}
+  try {return new ActiveXObject("Msxml2.XMLHTTP");}
+  catch (error) {}
+  try {return new ActiveXObject("Microsoft.XMLHTTP");}
+  catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
